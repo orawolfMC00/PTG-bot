21e817f57b12f686a3603b5f4b2ff7174a9af726
orawolfMC00 <99833077+orawolfMC00@users.noreply.github.com>
2022-03-20 09:44:39 +0100

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -8,6 +8,15 @@ client.on("messageCreate", async (message) => {
     )
         return;
 
+    if(message.mentions.has(client.user)){
+            let aideEmbed = new MessageEmbed()
+            .setTitle("Bienvenue")
+            .setDescription(`Bonjour / bonsoir ! Depuis le 19.03.2022, j'utilise désormais uniquement les commands slashs ! pour voir les commandes disponibles, fais /help !`)
+            .setColor("WHITE")
+            .setFooter({text: `Mentionné par ${message.author.username}`})
+            await message.reply({embeds: [aideEmbed]})
+        }
+    
     const [cmd, ...args] = message.content
         .slice(client.config.prefix.length)
         .trim()
